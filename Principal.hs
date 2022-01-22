ba7d48e1cc02dcc32004fb4a914339d748466e51
musictesterUS <musictester21@outlook.com>
2022-01-22 10:40:00 +0100

    añadido main del programa

---
+++ Principal.hs
@@ -0,0 +1,18 @@
+import System.IO
+import System.Directory
+import Control.Exception (catch, SomeException)
+import Proyecto.Recomendacionporartista
+
+main :: IO ()
+main = do
+  hSetBuffering stdout NoBuffering
+  putStrLn "Bienvenido a nuestro programa de recomendación de música"
+  putStrLn "Que desea hacer ?"
+  putStrLn "1. recomendar por artista"
+  putStrLn "2. ...."
+  putStrLn "3. ...."
+  o <- getChar 
+  if o == '1' then do 
+      putStrLn "Opción 1: "
+  else do 
+      putStrLn "Otra opción"
