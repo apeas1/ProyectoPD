59b73262fc661e14813ec5f9bd9baadbc7b87593
Miguel <miguelmuba01@gmail.com>
2022-01-22 11:45:40 +0100

    Recomendacion por artista está casi

--- funciones/recomendacionPorArtista.hs
+++ funciones/recomendacionPorArtista.hs
@@ -1,5 +1,5 @@
--- module Proyecto.Recomendacionporartista
--- ()where
+module Proyecto.Recomendacionporartista
+()where
 
 import Test.QuickCheck
 import Data.List
@@ -7,33 +7,31 @@ import qualified Data.Map as Map
 import qualified Data.Set as Set
 -- Relacionar artistas entre si por los usuarios que les escuchan
 -- Lista de artistas
-
--- Hace falta sacar la lista de artistas por usuario pero para eso hace falta definir bien el tipo Usuario
--- Sacar una lista con todos los artistas
-artistasTotal :: Eq a => [[a]] -> [a]
-artistasTotal xss = artistasAux xss []
-
-artistasAux [] ys = ys
-artistasAux (xs:xss) ys = artistasAux xss (artistasUsuario xs ys)
-
-artistasUsuario :: Eq a => [a] -> [a] -> [a]
-artistasUsuario (x:xs) aux
-    | aux == [] = xs
-    | elem x aux = artistasUsuario xs aux
-    | otherwise = artistasUsuario xs (x:aux)
+-----------------------------------------------------------------------------------------------------------------------------------------
+--                                     La función recomendados ejecuta el resto del archivo                                            --
+-----------------------------------------------------------------------------------------------------------------------------------------
 
 
+-- Hace falta una función que cree el diccionario (usuario, [artistas favoritos]) pero para eso hace falta tener bien definido el tipo usuario
+-- ejemplo para probar las funciones
+m1 = Map.fromList [(1,["AA","BB","CC","DD"]), (2,["BB","AA"]), (3, ["AA"]), (4,["BB"]), (5,[]), (6, ["CC"]), (7, ["CC", "DD"]),(8, ["CC", "DD"]), (9,["DD"])] 
 
--- Hacer una  lista de listas con los usuarios que escuchan a cada artista
+-- dictArtistaOyente m1 = [("AA",[1,2,3]),("BB",[1,2,4]),("CC",[1,6,7,8]),("DD",[1,7,8,9])]
+-- recomendados m1 = fromList [(1,[]),(2,["CC","DD"]),(3,["BB","CC","DD"]),(4,["AA","CC","DD"]),(5,[]),(6,["DD"]),(7,[]),(8,[]),(9,["CC"])]
 
--- Comparar los oyentes de cada artista para ver cuantos tienen en común
--- Teniendo un Map de artistas con sus oyentes
+-- Primero: crear a partir de este diccionario uno cuyas claves son los artistas y valores la lista de sus oyentes [(Artista,[Usuario])]
 
--- ejemplos para probar
-m1 = Map.fromList [("BB",[1,4,2]),("AA",[1,2,3]), ("CC", [1, 8, 7, 6]), ("DD", [8, 7 , 9 , 1])]
-mU = Map.fromList [(1,["AA","BB","CC","DD"]), (2,["BB","AA"]), (3, ["AA"]), (4,["BB"]), (5,[]), (6, ["CC"]), (7, ["CC", "DD"]),(8, ["CC", "DD"]), (9,["DD"])] 
-l1 = ["AA", "BB","DD"]
--- --------------------------------------------------------------------------------------------------------------------------
+dictArtistaOyente m = Map.fromList [(x, (oyentes x m)) | x <- xs]
+    where xs = artistasTotal m
+
+artistasTotal m = Set.toList ( Set.fromList (concat ( Map.elems m)))
+
+
+
+oyentes x m = [k | (k,v) <- (Map.toList m), elem x v]
+
+-- Segundo: comparar los oyentes de cada artista para ver cuantos tienen en común
+
 oyentesIguales [] ys = 0
 oyentesIguales  (x:xs) ys
     | elem x ys = 1 + oyentesIguales xs ys
@@ -45,19 +43,18 @@ comparacionOyentes xs yss = [(oyentesIguales xs ys) / l | ys <- yss]
 dicEnComun xs ys = Map.fromList (zip xs ys)
 
 filtrado m = Map.keys (Map.filter (>0.3) m )
-
+-- Tercero: recoger las relaciones entre artistas en un diccionario
 --Esto nos devuelve un diccionario de un artista y sus artistas relacionados a partir de un diccionario de un artista y sus oyentes
 relaciones m = Map.fromList [(k, filtrado (dicEnComun (delete k ks) (comparacionOyentes e (delete e es)))) | (k,e) <- (Map.toList m)]
     where ks = Map.keys m
           es = Map.elems m
 
--- Recomendar a un usuario los artistas relacionados con sus artistas favoritos que aun no escuche
 
--- Funcion que, devuelva un diccionario de un usuario y sus artistas preferidos
+-- Cuarto:Recomendar a un usuario los artistas relacionados con sus artistas favoritos que aun no escuche.
+-- A partir del diccionario original devolver un diccionario de usuario con los artistas recomendados
 
--- A partir de este diccionario y del diccionario que relaciona artistas devolver un diccionario de usuario con los artistas recomendados
-
-recomendados m1 m2 = Map.fromList [(k, obtenerRelacionados v m2) | (k,v) <- Map.toList m1 ]
+recomendados m1  = Map.fromList [(k, obtenerRelacionados v m2) | (k,v) <- Map.toList m1 ]
+    where m2 = relaciones  (dictArtistaOyente m1)
 
 obtenerRelacionados  xs m = (Set.toList ( Set.fromList (concat [v | (k,v) <- (Map.toList m), (elem k xs)] ))) \\ xs
 
